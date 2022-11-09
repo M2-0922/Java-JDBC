213dbef13410a97e697290521978f8393469831b
Aya <pyon220@gmail.com>
2022-11-08 17:06:18 -0800

    spring web application h2-jpa homework finished

--- spring-web-application-h2-jpa/src/main/java/com/kubilayckmk/springwebapplicationh2jpa/controller/EmpController.java
+++ spring-web-application-h2-jpa/src/main/java/com/kubilayckmk/springwebapplicationh2jpa/controller/EmpController.java
@@ -9,26 +9,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.kubilayckmk.springwebapplicationh2jpa.dao.iEmployeeRepository;
 import com.kubilayckmk.springwebapplicationh2jpa.dao.iProjectRepository;
-import com.kubilayckmk.springwebapplicationh2jpa.entity.Project;
+import com.kubilayckmk.springwebapplicationh2jpa.entity.Employee;
+
 
 @Controller
-@RequestMapping("/project")
-public class ProjectController {
+@RequestMapping("/employee")
+public class EmpController {
 
     // Autowired is used to inject the dependency by type
     @Autowired
-    iProjectRepository projectRepository;
-    
-    @GetMapping("/new")
-    public String displayProjectForm(Model model){
-        model.addAttribute("project", new Project());
-        return "project/new-project";
+    iEmployeeRepository employeeRepository;
+
+    @GetMapping("/addEmployee")
+    public String displayEmpForm(Model model){
+        model.addAttribute("employee", new Employee());
+        return "project/new-employee";
     }
 
     @PostMapping("/save")
-    public String createProject(Project project, Model model){
-        projectRepository.save(project);
-        return "redirect:/project/new";
+    public String createProject(Employee employee, Model model){
+      employeeRepository.save(employee);
+        return "redirect:/employee/addEmployee";
     }
 }
