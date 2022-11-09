68fab028ac73957bd92d5b8c38267248ee0602e7
Mateus <mateuscgr@gmail.com>
2022-11-09 14:07:20 -0300

    Finished

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,13 @@
-package com.kubilayckmk.springwebapplication1;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringWebApplication1Application {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringWebApplication1Application.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
--- demo/src/main/java/com/example/demo/ServletInitializer.java
+++ demo/src/main/java/com/example/demo/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.kubilayckmk.springwebapplication1;
+package com.example.demo;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(SpringWebApplication1Application.class);
+		return application.sources(DemoApplication.class);
 	}
 
 }
